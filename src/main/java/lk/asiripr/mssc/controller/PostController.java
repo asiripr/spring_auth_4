584dd0c24227bbe501f63de6c108abae5175d8d8
asiripr <asiripramodaya2000@gmail.com>
2024-10-02 07:47:31 +0530

    fixed some issues

--- src/main/java/lk/asiripr/mssc/controller/PostController.java
+++ src/main/java/lk/asiripr/mssc/controller/PostController.java
@@ -1,17 +1,26 @@
 package lk.asiripr.mssc.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lk.asiripr.mssc.model.Post;
 import lk.asiripr.mssc.repository.PostRepository;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/posts")
 public class PostController {
 	private final PostRepository repository;
 
 	public PostController(PostRepository repository) {
 		this.repository = repository;
+	}
+	
+	@GetMapping
+	public List<Post> findAll(){
+		return repository.findAll();
 	}
 
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,8 @@
-craete table post(
-	id int auto_increment primary key,
-	title varchar(255) not null,
-	content text not null
+create table Post (
+
+id int auto_increment primary key ,
+title varchar(255) not null,
+content text not null
 );
 
-INSERT INTO POST (title, content) VALUES ('Asiri Pramodaya','Backend Developer');
+INSERT INTO POST(title, content) VALUES ('Hello, World!', 'My First Blog Post');
